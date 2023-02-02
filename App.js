9bdb7338e911eeed8eb960dbd060d4c030079cb7
Delfi <delfinamunich@gmail.com>
2023-02-02 18:38:32 -0300

    demo terminada

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { StyleSheet, Text, View, Button , Image, TextInput, FlatList} from 'reac
 
 export default function App() {
 
+  const libros = require('./libros.json')
   const [input , setInput] = useState('')
   const [opciones, setOpciones] = useState([])
   const [resultado, setResultado] = useState('')
@@ -12,10 +13,18 @@ export default function App() {
   const addtoArray = () => {
     opciones.push(input)
     console.log(opciones)
+    console.log(libros[1].Title)
   }
-  const handleChange = () =>{
+  const randomCategoria = () =>{
     const random = Math.floor(Math.random() * opciones.length)
     setResultado(opciones[random].toString())
+    console.log(resultado)
+  }
+
+  const randomLibro = () =>{
+    const random = Math.floor(Math.random() * libros.length)
+    const libro = libros[random]
+    setResultado(libro.Title.toString() + ' de ' + libro.Author.toString())
     console.log(resultado)
   }
   return (
@@ -23,9 +32,10 @@ export default function App() {
 <TextInput onChangeText={text => setInput(text)} value={input} style={{backgroundColor: 'pink'}}></TextInput>
       <Button onPress={addtoArray} title="Add"></Button>
       <Text style={{padding: 10, fontSize: 42}}>
-        {opciones.toString().split(',').join(' ')}
+        {opciones.toString().split(',').join(' \n')}
       </Text>
- <Button onPress={handleChange} title="Change"></Button>
+ <Button onPress={randomCategoria} title="random por categoría"></Button>
+  <Button onPress={randomLibro} title="random por libro"></Button>
 <Text>{resultado}</Text>
       <StatusBar style="auto" />
     </View>
